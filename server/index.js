962c04b94facc27d96ace4387f5193b6b7dad19e
lensherrggg <gr98522@gmail.com>
2020-03-28 21:06:24 +0800

    Authentication with oauth phase 1 finished

--- server/index.js
+++ server/index.js
@@ -1,9 +1,24 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20');
+const keys = require('./config/keys');
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there'});
-});
+passport.use(new GoogleStrategy({
+  clientID: keys.googleClientID,
+  clientSecret: keys.googleClientSecret,
+  callbackURL: '/auth/google/callback'
+}, accessToken => {
+  console.log(accessToken)
+}));
+
+app.get('/auth/google', passport.authenticate('google', {
+  scope: ['profile', 'email']
+  })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
 
