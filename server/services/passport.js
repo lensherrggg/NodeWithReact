b5ff26bde84bd650168ed688b1c73e72b08dbf74
lensherrggg <gr98522@gmail.com>
2020-04-01 21:43:40 +0800

    Client page updated

--- server/services/passport.js
+++ server/services/passport.js
@@ -21,17 +21,15 @@ passport.use(new GoogleStrategy({
   clientSecret: keys.googleClientSecret,
   callbackURL: '/auth/google/callback',
   proxy: true
-}, ( accessToken, refreshToken, profile, done ) => {
-  User.findOne({ googleId: profile.id })
-    .then((existingUser) => {
-      if (existingUser) {
-        // we already have a record with the given profile ID
-        done(null, existingUser);
-      } else {
-        // we dont have a user record with this Id, make a new record
-        new User({ googleId: profile.id })
-          .save()
-          .then(user => done(null, user));
-      }
-    })
-}));
+  }, 
+  async ( accessToken, refreshToken, profile, done ) => {
+    const existingUser =  await User.findOne({ googleId: profile.id });
+      
+    if (existingUser) {
+      return done(null, existingUser);
+    }
+    const user = await new User({ googleId: profile.id }).save()
+    done(null, user);
+    }
+  )
+)
